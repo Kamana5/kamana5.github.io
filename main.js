f0dcb464f38e45023d6e4d401512eec658c29730
Kamana Chapagain <141091218+Kamana5@users.noreply.github.com>
2025-08-14 14:57:01 -0400

    Updated Version
    Updated experience, projects and images.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// typing effect for the words "Developer.", "Engineer.", "Programmer."
 document.addEventListener("DOMContentLoaded", function() {
     const words = ["Developer.", "Engineer.", "Programmer."];
     let currentWordIndex = 0;
@@ -31,4 +32,40 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     type();
+});
+
+// Carousel functionality for the image carousels
+document.querySelectorAll('.next-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        const carousel = button.previousElementSibling;
+        const images = carousel.querySelectorAll('img');
+        let current = Array.from(images).findIndex(img => img.style.display !== 'none');
+        
+        images [current].style.display = 'none';
+        let next = (current + 1) % images.length;
+        images[next].style.display = 'block';
+    });
+});
+
+// Fetching the different pages for the portfolio section
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("components/header.html")
+    .then(res => {
+      if (!res.ok) throw new Error("Header not found");
+      return res.text();
+    })
+    .then(data => {
+      document.getElementById("header-container").innerHTML = data;
+    })
+    .catch(err => {
+      console.error("Error loading header:", err);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("components/footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer-container").innerHTML = data;
+    });
 });
